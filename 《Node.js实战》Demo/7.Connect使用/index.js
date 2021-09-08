c1520ded655b4bed0c9e7ffbaecec892b0c20a83
ZiKng-Coding <3193373997@qq.com>
2021-09-09 05:17:01 +0800

    feat

--- 《Node.js实战》Demo/7.Connect使用/index.js
+++ 《Node.js实战》Demo/7.Connect使用/index.js
@@ -17,4 +17,40 @@ function hello(req, res) {
   res.end('Hello World')
 }
 
-connect().use(logger).use(hello).listen(3000)
+
+// 实现HTTP Basic认证的中间件组件
+function restrict(req, res, next) {
+  let authorization = req.header.authorization;
+  if(!authorization) return next(new Error('Invalid authorization'))
+
+  let parts = authorization.split(' ')
+  let scheme = parts[0]
+  let auth = new Buffer(parts[1], 'base64').toString().split(':')
+  let user = auth[0]
+  let pass = auth[1]
+
+  authenticateWithDatabase(user, pass, err => {
+    if(err) return next(err)
+    next();
+  })
+}
+
+function admin(req, res, next) {
+  switch (req.url) {
+    case '/':
+      res.end('try/users')
+      break
+    case '/users':
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify(['tobi', 'sdf', 'sdfs']))
+      break
+  }
+}
+
+// 挂载
+connect()
+  .use(logger)
+  .use('/admin', restrict)
+  .use('admin', admin)
+  .use(hello)
+  .listen(3000)
